293639d542a1dd6a71aacf203692323be01ce00c
Andres <andres_marin1992@hotmail.com>
2025-04-04 20:25:36 -0500

    Add additional humorous commit messages

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ const messages = [
     "Reinventing the wheel. Again.",
     "This is not the commit message you are looking for",
     "Batman! (this commit has no parents)",
+    "Muy suaves",
+    "This commit is not a bug, it's a feature",
   ];
   
   const funnyCommit = () => {
